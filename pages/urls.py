cf715794a1e69949c1d13c8d32ead460919f3276
Nicole Hardy <nichardy@bu.edu>
2019-11-03 18:33:12 -0500

    Added Template to Pages App

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 #pages/ur;s.py
 
 from django.urls import path
-from .views import homePageView #response function
+from .views import HomePageView, AboutPageView #class def
 
 urlpatterns = [
     # map url in empty string to function homepageview
-    path('', homePageView, name='home')
+    #path('', homePageView, name='home') function based view
+    path('', HomePageView.as_view(), name='home'), # generic class based view
+    path('about/', AboutPageView.as_view(), name='about'), # generic class based view
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse #creates a response to url request within this file
-# Create your views here.
+from django.views.generic import TemplateView # base class for a generic view
+#eate your views here.
 
+class HomePageView(TemplateView):
+    ''' Inherit from the generic templateview to yse an external HTML template file.'''
+    template_name = 'pages/home.html'
 
-def homePageView(request):
-    """ responds to http request and returns an HttpResponse"""
+class AboutPageView(TemplateView):
+    ''' Inherit from the generic templateview to yse an external HTML template file.'''
+    template_name = 'pages/about.html'
 
-    s = ''' 
-    <html>
-    <head>
-        <title>Hello, world!</title>
-    </head>
-    <body>
-    <h1>Hello, world!</h1>
-    Welcome to our first Django application!
-    <p>
-    </body>
-    </html> 
-    '''
-
-    return HttpResponse(s)
+#def homePageView(request):
+#    """ responds to http request and returns an HttpResponse"""
+#
+#    s = ''' 
+#    <html>
+#    <head>
+#        <title>Hello, world!</title>
+#    </head>
+#    <body>
+#    <h1>Hello, world!</h1>
+#    Welcome to our first Django application!
+#    <p>
+#    </body>
+#    </html> 
+#    '''
+#
+#    return HttpResponse(s)
