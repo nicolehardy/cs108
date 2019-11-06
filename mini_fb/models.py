df7151a57dfd9bfde38ce04dd217843b74c7f73f
Nicole Hardy <nichardy@bu.edu>
2019-11-06 16:27:50 -0500

    Assignment 16

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    """ stores information for each profile """
+    firstname = models.TextField()
+    lastname = models.TextField()
+    city = models.TextField()
+    email = models.TextField()
+    image_url = models.URLField()
+
+    def __str__(self):
+        """returns string representation of the object """
+
+        return "%s %s       %s" %(self.firstname, self.lastname, self.city)
